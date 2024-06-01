b24f2dbdbf4e905debce4f0270e63153278efeb0
Refaldy Bagas Anggana <refaldybanggana@gmail.com>
2024-06-01 17:30:17 +0700

    using eloquent

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,38 +2,12 @@
 
 namespace App\Models;
 
-use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-class Post
+class Post extends Model
 {
-    public static function all()
-    {
-        return [
-            [
-                'id' => '1',
-                'slug' => 'judul-artikel-1',
-                'title' => 'Judul Artikel 1',
-                'author' => 'Refaldy Bagas A.',
-                'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate aliquid totam possimus eum ipsam voluptas! Assumenda nihil quasi alias sunt tempora! Maiores magnam officia enim ex nisi necessitatibus illo quas?'
-            ],
-            [
-                'id' => '2',
-                'slug' => 'judul-artikel-2',
-                'title' => 'Judul Artikel 2',
-                'author' => 'Refaldy Bagas A.',
-                'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate aliquid totam possimus eum ipsam voluptas! Assumenda nihil quasi alias sunt tempora! Maiores magnam officia enim ex nisi necessitatibus illo quas?'
-            ]
-        ];
-    }
+    use HasFactory;
 
-    public static function find($slug) : array
-    {
-        $post = Arr::first(static::all(), fn($post) => $post['slug'] == $slug);
-
-        if (!$post) {
-            abort(404);
-        }
-
-        return $post;
-    }
+    protected $fillable = ['title', 'author', 'slug', 'body'];
 }
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,9 +3,9 @@
 
     <article class="py-8 max-w-screen-md">
         <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">{{ $post['title'] }}</h2>
-        
+
         <div class="text-base text-gray-500">
-            <a href="#">{{ $post['author'] }} | 08 May 2024</a>
+            <a href="#">{{ $post['author'] }} | {{ $post->created_at->diffForHumans() }}</a>
         </div>
         <p class="my-4 font-light">{{ $post['body'] }}</p>
         <a href="/posts" class="font-medium text-blue-500 hover:underline">&laquo; Back to posts</a>
